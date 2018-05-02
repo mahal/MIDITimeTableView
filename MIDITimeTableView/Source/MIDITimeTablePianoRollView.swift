c7c96fff67b5f3920a4bc9015e31a778fee77cff
mahal <mahal@raskinformac.com>
2018-05-02 18:37:41 +0200

    make pianoroll scrollable on note cells

--- MIDITimeTableView/Source/MIDITimeTablePianoRollView.swift
+++ MIDITimeTableView/Source/MIDITimeTablePianoRollView.swift
@@ -3,11 +3,47 @@
 //  MIDITimeTableView
 //
 //  Created by Martin Halter on 02.05.18.
-//  Copyright © 2018 cemolcay. All rights reserved.
+//  Copyright © 2018 Raskin Software LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 open class MIDITimeTablePianoRollView: MIDITimeTableViewBase {
+
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+  
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+  
+  private func commonInit() {
+    canCancelContentTouches = true
+    delaysContentTouches = false
+  }
+  
+  override open func touchesShouldCancel(in view: UIView) -> Bool {    
+    if (view.isKind(of:MIDITimeTableCellView.self)) {
+      return true
+    }
+    return super.touchesShouldCancel(in: view)
+  }    
+  
+  override open func touchesShouldBegin(_ touches: Set<UITouch>, with event: UIEvent?, in view: UIView) -> Bool {
+    // this is intermediate right. It may change when we do zooming. 
+    return false
+    
+    let touch = touches.first
+    if (touch?.phase == UITouchPhase.moved) {
+      if (view.isKind(of:MIDITimeTableCellView.self)) {
+        return false
+      }
+    } 
+  
+    return super.touchesShouldBegin(touches, with: event, in: view)
+  }
 
 }
